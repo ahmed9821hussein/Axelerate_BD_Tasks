510c9ab5b2df6d6c6dc1fc0a16a3ca58babce2df
Ahmed Hussein <ahmed9821hussein@gmail.com>
2024-07-28 19:58:57 +0300

    Initial project setup
    Set up the basic project structure, including required dependencies, initial command implementation, and manifest file for the Revit add-in.

---
+++ Task1/CreateFloorCommand.cs
@@ -0,0 +1,19 @@
+﻿using Autodesk.Revit.Attributes;
+using Autodesk.Revit.DB;
+using Autodesk.Revit.UI;
+using System;
+
+namespace Task1
+{
+    [Transaction(TransactionMode.Manual)]
+    public class CreateFloorCommand : IExternalCommand
+    {
+        public Result Execute(
+          ExternalCommandData commandData,
+          ref string message,
+          ElementSet elements)
+        {
+            return Result.Succeeded;
+        }
+    }
+}
