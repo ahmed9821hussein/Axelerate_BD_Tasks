2cf81a58e64a3073a1df3d4997731de23b5f7d0e
Ahmed Hussein <ahmed9821hussein@gmail.com>
2024-07-28 21:50:25 +0300

    Implement level retrieval method
    Add GetLevel method in LevelExtensions class

---
+++ Task1/ExtensionMethods/LevelExtensions.cs
@@ -0,0 +1,17 @@
+﻿using Autodesk.Revit.DB;
+using System.Linq;
+
+namespace Task1.ExtensionMethods
+{
+
+    public static class LevelExtensions
+    {
+        public static Level GetLevel(Document doc)
+        {
+            FilteredElementCollector collector = new FilteredElementCollector(doc);
+            collector.OfClass(typeof(Level));
+            return collector.Cast<Level>().FirstOrDefault();
+        }
+    }
+
+}
