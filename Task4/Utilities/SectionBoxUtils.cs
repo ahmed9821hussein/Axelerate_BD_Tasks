0608cec375e58bd98128a6753987f55443161505
Ahmed Hussein <ahmed9821hussein@gmail.com>
2024-07-29 16:56:06 +0300

    Task 4 Setting the Project

---
+++ Task4/Utilities/SectionBoxUtils.cs
@@ -0,0 +1,34 @@
+﻿using Autodesk.Revit.DB;
+using System;
+
+namespace Task4.Utilities
+{
+    public static class SectionBoxUtils
+    {
+        public static void ModifySectionBoxDepth(Document doc, View3D view, double additionalHeightInFeet)
+        {
+            if (view == null || !view.IsSectionBoxActive)
+            {
+                throw new InvalidOperationException("The active view does not have an active section box.");
+            }
+
+            using (Transaction trans = new Transaction(doc, "Modify Section Box Depth"))
+            {
+                trans.Start();
+
+                BoundingBoxXYZ sectionBox = view.GetSectionBox();
+                if (sectionBox != null)
+                {
+                    double additionalHeightInMM = additionalHeightInFeet * 0.3048;
+
+                    sectionBox.Max = new XYZ(sectionBox.Max.X, sectionBox.Max.Y, sectionBox.Max.Z + additionalHeightInMM);
+                    sectionBox.Min = new XYZ(sectionBox.Min.X, sectionBox.Min.Y, sectionBox.Min.Z - additionalHeightInMM);
+
+                    view.SetSectionBox(sectionBox);
+                }
+
+                trans.Commit();
+            }
+        }
+    }
+}
