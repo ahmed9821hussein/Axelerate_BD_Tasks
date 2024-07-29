4cb82b2f4d066b51307685514bb1580d362a9f6d
Ahmed Hussein <ahmed9821hussein@gmail.com>
2024-07-29 07:23:23 +0300

    Add family instance utility methods
    - Implemented FamilyInstanceUtils with methods to handle family instances

--- Task2/Utilities/FamilyInstanceUtils.cs
+++ Task2/Utilities/FamilyInstanceUtils.cs
@@ -1,12 +1,73 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using Autodesk.Revit.DB;
+using Autodesk.Revit.DB.Architecture;
+using Autodesk.Revit.UI;
+using System;
 
 namespace Task2.Utilities
 {
-    internal class FamilyInstanceUtils
+    public static class FamilyInstanceUtils
     {
+        public static void HandleFamilyOrientation(Document doc, FamilyInstance familyInstance, XYZ orientation, Curve wallCurve, XYZ location, XYZ roomMidPoint)
+        {
+            if (!familyInstance.FacingOrientation.IsAlmostEqualTo(orientation))
+            {
+                familyInstance.flipFacing();
+            }
+
+            if (GeoUtils.IsVerticalCurve(wallCurve))
+            {
+                VerticalOrientationCriteria(familyInstance, wallCurve, location, roomMidPoint, doc);
+            }
+            else
+            {
+                HorizontalOrientationCriteria(familyInstance, wallCurve, location, roomMidPoint, doc);
+            }
+        }
+
+        private static void VerticalOrientationCriteria(FamilyInstance familyInstance, Curve wallCurve, XYZ location, XYZ roomMidPoint, Document doc)
+        {
+            XYZ direction = location - roomMidPoint;
+            bool flipRequired = Math.Abs(wallCurve.GetEndPoint(0).X - wallCurve.GetEndPoint(1).X) < 0.001;
+
+            if (flipRequired && direction.Y < 0)
+            {
+                if (!familyInstance.HandOrientation.IsAlmostEqualTo(new XYZ(0, 1, 0)))
+                {
+                    familyInstance.flipHand();
+                }
+                ElementTransformUtils.MoveElement(doc, familyInstance.Id, new XYZ(0, 1.5, 0));
+            }
+            else if (flipRequired && direction.Y >= 0)
+            {
+                if (!familyInstance.HandOrientation.IsAlmostEqualTo(new XYZ(0, -1, 0)))
+                {
+                    familyInstance.flipHand();
+                }
+                ElementTransformUtils.MoveElement(doc, familyInstance.Id, new XYZ(0, -1.5, 0));
+            }
+        }
+
+        private static void HorizontalOrientationCriteria(FamilyInstance familyInstance, Curve wallCurve, XYZ location, XYZ roomMidPoint, Document doc)
+        {
+            XYZ direction = location - roomMidPoint;
+            bool flipRequired = Math.Abs(wallCurve.GetEndPoint(0).Y - wallCurve.GetEndPoint(1).Y) < 0.001;
+
+            if (flipRequired && direction.X < roomMidPoint.X)
+            {
+                if (!familyInstance.HandOrientation.IsAlmostEqualTo(new XYZ(1, 0, 0)))
+                {
+                    familyInstance.flipHand();
+                }
+                ElementTransformUtils.MoveElement(doc, familyInstance.Id, new XYZ(1.5, 0, 0));
+            }
+            else if (flipRequired && direction.X >= roomMidPoint.X)
+            {
+                if (!familyInstance.HandOrientation.IsAlmostEqualTo(new XYZ(-1, 0, 0)))
+                {
+                    familyInstance.flipHand();
+                }
+                ElementTransformUtils.MoveElement(doc, familyInstance.Id, new XYZ(-1.5, 0, 0));
+            }
+        }
     }
 }
